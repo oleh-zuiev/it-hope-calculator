4028fc376aa4097318a5bc7a94d1b177cbd38af6
oleh-zuiev <64410055+oleh-zuiev@users.noreply.github.com>
2022-01-22 22:06:19 +0200

    percent button
    adds percent caculating button

--- JS/main.js
+++ JS/main.js
@@ -40,6 +40,25 @@ switch (actionSign) {
     resultScreenRef.textContent = finalResult;
     
 }
+const calculatePercent = function () {
+    console.log('calculating percent');
+    numOne = Number(numOne);
+    numTwo = Number(numTwo);    
+    let finalResultPercent;
+    finalResultPercent = numOne / 100 * numTwo;
+    if (Number.isInteger(finalResultPercent)) {
+    resultScreenRef.textContent = finalResultPercent;        
+    } else {
+    const fixedResult = finalResultPercent.toFixed(7);
+        console.log(fixedResult);
+        const noZero = fixedResult.toString();
+        console.log(noZero);
+        const answerPercent = Number(noZero);
+    resultScreenRef.textContent = answerPercent; 
+    }
+   
+
+}
 btnContainerRef.addEventListener('click', function (e) {
     if (!e.target.classList.contains('btn')) {
         return;   
@@ -49,8 +68,9 @@ btnContainerRef.addEventListener('click', function (e) {
         clearScreen();
     }
     const eTxt = e.target.textContent;
-    if (numTwo === '' && actionSign === ''&& numerals.includes(eTxt)) {
+    if (numTwo === '' && actionSign === ''&& numerals.includes(eTxt)&&numOne.length<7) {
         numOne += eTxt;
+        // Number.parseInt( "123FA", 10 );-прописать условие через isInteger, чтобы избав. от leading zero
     resultScreenRef.textContent = numOne;
 
     }
@@ -59,12 +79,18 @@ btnContainerRef.addEventListener('click', function (e) {
         actionSign = eTxt;
     resultScreenRef.textContent = actionSign;
     }
-    if (numOne !== '' && actionSign !== '' && numerals.includes(eTxt)) {
+    if (numOne !== '' && actionSign !== '' && numerals.includes(eTxt)&&numTwo.length<7) {
         numTwo += eTxt;
         resultScreenRef.textContent = numTwo;
     }
     if (numOne !== '' && actionSign !== '' && numTwo !== '' && eTxt === '=') {
         calculateResult();
+        numOne = '';
+        numTwo = '';
+        actionSign = '';
+    }
+    if (numOne !== '' && actionSign === 'X' && numTwo !== '' && eTxt === '%') {
+        calculatePercent();
         numOne = '';
         numTwo = '';
         actionSign = '';
